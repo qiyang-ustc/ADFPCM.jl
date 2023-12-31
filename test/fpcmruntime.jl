6a721e3cd532450026230ab5ac6063770bdac3da
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2023-12-31 10:00:25 +0800

    fix test

--- test/fpcmruntime.jl
+++ test/fpcmruntime.jl
@@ -86,6 +86,8 @@ end
     M[1,1,1,2]=1.0
     M = atype(M)
     χ = 16
-    rt = FPCM(M, ADFPCM.Params(χ=χ, ifsave=false))
+    params = ADFPCM.Params(χ=χ, tol=1e-10, ifsave=false)
+    rt = initialize_runtime(M, params)
+    rt = FPCM(rt, params)
     @test logZ(rt) ≈ 0.29152163577 atol = 1e-4
 end
