1189b8b106e88b55a8e21ca36cbd39cde584ec1d
XingyuZhang2018 <zhangxingyu@iphy.ac.cn>
2024-01-29 20:24:29 +0800

    rejoinpath for tensorkit

--- src/tensorkitpatch.jl
+++ src/tensorkitpatch.jl
@@ -2,3 +2,6 @@ import Base.Array
 
 Array(A::AbstractTensorMap) = (A.data .= Array(A.data); A)
 CuArray(A::AbstractTensorMap) = (A.data .= CuArray(A.data); A)
+
+regularform(S::String) = replace(S, "=>" => "→")
+rejoinpath(S::String...) = regularform(Base.joinpath(S))
